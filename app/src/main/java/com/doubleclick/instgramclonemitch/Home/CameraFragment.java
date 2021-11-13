adc163647c507cdec1f34273d1fdb2698b94835f
Eslam Ghazy <46050044+CoderEslam@users.noreply.github.com>
2021-11-13 22:24:54 +0200

    InstgramCloneM

---
+++ app/src/main/java/com/doubleclick/instgramclonemitch/Home/CameraFragment.java
@@ -0,0 +1,30 @@
+package com.doubleclick.instgramclonemitch.Home;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.doubleclick.instgramclonemitch.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CameraFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CameraFragment extends Fragment {
+
+    private static final String TAG = "CameraFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_camera, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/doubleclick/instgramclonemitch/Likes/LikesActivity.java
@@ -0,0 +1,44 @@
+package com.doubleclick.instgramclonemitch.Likes;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.doubleclick.instgramclonemitch.R;
+import com.doubleclick.instgramclonemitch.Utils.BottomNavigationViewHelper;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class LikesActivity extends AppCompatActivity {
+
+    private static final String TAG = "LikesActivity";
+    private static final int ACTIVITY_NUM = 3;
+
+    private Context mContext = LikesActivity.this;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Log.d(TAG, "onCreate: started.");
+
+        setupBottomNavigationView();
+    }
+
+    /**
+     * BottomNavigationView setup
+     */
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationView bottomNavigationViewEx =  findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(mContext, this,bottomNavigationViewEx);
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
+    }
+}
